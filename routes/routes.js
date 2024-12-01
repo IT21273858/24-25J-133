e814d2e6c4b6e7adbefe821a698381670a9ac888
IT21273858 <it21273858@my.sliit.lk>
2024-12-01 18:14:18 +0530

    parents functionality implemented

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,7 @@ const upload = multer({ dest: "uploads/" });
 const express = require('express');
 const imagePredictionController = require('../src/controllers/imagePrediction')
 const patternPredictionController = require('../src/controllers/patternPrediction')
+const ParentController = require('../src/controllers/parentController')
 const router = express.Router();
 
 // Image Prediction Routes
@@ -10,6 +11,16 @@ router.post("/predict-shape", upload.single("image"), imagePredictionController.
 
 // Pattern Prediction Route
 router.post("/predict-pattern", patternPredictionController.getPatternPrediction);
+
+// Parent Route
+router.get('/parents/getAll', ParentController.getAllParents);
+router.get('/parents/get/:id', ParentController.getParentById);
+router.post('/parents/create', ParentController.createParent);
+router.patch('/parents/update/:id', ParentController.updateParent);
+router.delete('/parents/delete/:id', ParentController.deleteParent);
+router.post('/parents/login', ParentController.login);
+router.post('/parents/forgot', ParentController.forgotParent);
+router.post('/parents/verifyotp', ParentController.verifyotp);
 
 module.exports = router;
 
